1b9069f42cd658a9861a4cf35e41f076656f8d53
danielye <danielye@tencent.com>
2023-06-21 15:40:57 +0800

    Fix debug build (w/ gcc 5.5.0) on Linux X86

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -4616,7 +4616,8 @@ public:
   void set_processed_query(const char *query_arg, size_t query_length_arg)
   {
     mysql_mutex_lock(&LOCK_thd_query);
-    m_processed_query_string = { query_arg, query_length_arg };
+    m_processed_query_string.str = query_arg;
+    m_processed_query_string.length = query_length_arg;
     mysql_mutex_unlock(&LOCK_thd_query);
   }
   void set_query(const LEX_CSTRING& query_arg);
