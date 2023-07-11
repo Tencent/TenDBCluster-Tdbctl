d8c9f0bb206582c09dc6da043aff6f05ce381d5f
stellanduan <stellanduan@tencent.com>
2023-07-11 15:26:13 +0800

    No need to send [reset sql_command] to spider node

--- sql/tc_base.cc
+++ sql/tc_base.cc
@@ -1955,6 +1955,7 @@ bool tc_command_convert(THD *thd, LEX *lex, TC_PARSE_RESULT *tc_parse_result_t)
     case SQLCOM_SLAVE_STOP:
     case SQLCOM_INSTALL_PLUGIN:
     case SQLCOM_UNINSTALL_PLUGIN:
+    case SQLCOM_RESET:
       tc_parse_result_t->execute_flag |= TC_TDBCTL_NEED_EXECUTE;
       break;
     // These commands are not supported in tcadmin primary or secondary mode.
@@ -2056,7 +2057,6 @@ bool tc_command_convert(THD *thd, LEX *lex, TC_PARSE_RESULT *tc_parse_result_t)
       tc_parse_result_t->execute_flag |= TC_TDBCTL_NEED_EXECUTE | TC_ONLY_ONE_SPIDER_NEED_EXECUTE;
       break;
     case SQLCOM_SET_OPTION:
-    case SQLCOM_RESET:
       // if the sys_var is tdbctl var, we only execute it on tdbctl itself
       while ((var = var_it++))
       {
