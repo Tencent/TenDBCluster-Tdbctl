bfd177a2dc5abe192fe913ad875c7da6e1a84dfc
hanzaixiao <hanzaixiao@tencent.com>
2025-12-22 19:12:38 +0800

    bugfix: Remove redundant header file includes. issue: #136

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -20,7 +20,6 @@
 #include "sql_lex.h"
 
 #include "mysql_version.h"             // MYSQL_VERSION_ID
-#include "mysql_version.h.in"
 #include "sp_head.h"                   // sp_head
 #include "sql_class.h"                 // THD
 #include "sql_parse.h"                 // add_to_list
